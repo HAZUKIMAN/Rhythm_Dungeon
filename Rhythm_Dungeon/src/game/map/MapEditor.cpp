d7dd0c807e254caf70a60f4aaebaaf95f518b983
ギター・ヒーロー <hazuki.rui.aoi@gmail.com>
2026-04-23 12:49:10 +0900

    qwe

--- Rhythm_Dungeon/src/game/map/MapEditor.cpp
+++ Rhythm_Dungeon/src/game/map/MapEditor.cpp
@@ -1,4 +1,20 @@
 #include"MapEditor.h"
+#include <corecrt_math.h>
+#include "../common.h"
+
+
+const int MAP_W = 50;
+const int MAP_H = 50;
+
+const float TILE_SIZE = 5.0f;
+
+enum TileType {
+    TILE_NONE,
+    TILE_FLOOR,
+    TILE_WALL
+};
+
+TileType map[MAP_H][MAP_W];
 
 // コンストラクタ
 MapEditor::MapEditor()
@@ -12,35 +28,230 @@ MapEditor::~MapEditor()
 }
 
 // 初期化
-void Init()
+void MapEditor::Init()
 {
 
 }
 
 
 // データロード
-void Load()
+void MapEditor::Load()
 {
 
 }
 
 
 // 実行処理
-int  Step()
+int  MapEditor::Step()
 {
 
+//------------------------------------
+//    データの保存・読み取り
+//------------------------------------
+    if (CheckHitKey(KEY_INPUT_S)) {
+        SaveMap("map.dat");
+    }
+
+    if (CheckHitKey(KEY_INPUT_L)) {
+        LoadMap("map.dat");
+    }
+   
+
+}
+// 更新処理
+void MapEditor::Update()
+{
+    //ビット
+    //MOUSE_INPUT_LEFT;   // 左クリック
+    //MOUSE_INPUT_RIGHT;  // 右クリック
+    //MOUSE_INPUT_MIDDLE; // 中クリック
+
+    static int prevMouseState = 0;
+
+    int mouseState = GetMouseInput();
+
+    VECTOR hitPos;
+
+    if (GetMouseHitPosition(&hitPos)) {
+
+        int gx, gz;
+
+        if (GetGridPos(hitPos, &gx, &gz)) {
+
+           
+            // 左クリック（押した瞬間）
+            //今押してる状態かつ前は押してない状態
+            if ((mouseState & MOUSE_INPUT_LEFT) &&
+                !(prevMouseState & MOUSE_INPUT_LEFT)) {
+
+                map[gz][gx] = TILE_FLOOR;
+            }
+
+            // 右クリック
+            //今押してる状態かつ前は押してない状態
+            if ((mouseState & MOUSE_INPUT_RIGHT) &&
+                !(prevMouseState & MOUSE_INPUT_RIGHT)) {
+
+                map[gz][gx] = TILE_WALL;
+            }
+
+            // 中クリック
+            //今押してる状態かつ前は押してない状態
+            if ((mouseState & MOUSE_INPUT_MIDDLE) &&
+                !(prevMouseState & MOUSE_INPUT_MIDDLE)) {
+
+                map[gz][gx] = TILE_NONE;
+            }
+        }
+    }
+
+    prevMouseState = mouseState;
 
 }
 
 // 描画
-void Draw()
+void MapEditor::Draw()
 {
+    //ブロックの表示
+    for (int z = 0; z < MAP_H; z++) {
+        for (int x = 0; x < MAP_W; x++) {
+
+            if (map[z][x] == TILE_FLOOR) {
+                DrawCube3D(
+                    VGet(x * 5, 0, z * 5),
+                    VGet(x * 5 + 5, 1, z * 5 + 5),
+                    GetColor(100, 100, 100), GetColor(100, 100, 100), TRUE);
+            }
+
+            if (map[z][x] == TILE_WALL) {
+                DrawCube3D(
+                    VGet(x * 5, 0, z * 5),
+                    VGet(x * 5 + 5, 5, z * 5 + 5),
+                    GetColor(200, 200, 200), GetColor(200, 200, 200), TRUE);
+            }
+        }
+    }
+
+    VECTOR hitPos;
+
+    //グリッドの表示
+    if (GetMouseHitPosition(&hitPos)) {
+        printf("Hit: %f %f %f\n", hitPos.x, hitPos.y, hitPos.z);
+
+        int gx, gz;
+        if (GetGridPos(hitPos, &gx, &gz)) {
+            map[gz][gx] = TILE_FLOOR;
+        }
+    }
+
+    DrawString(1400,50,"Sでセーブ\nLでロード",WHITE);
 
 }
 
 
 // 終了処理
-void Fin()
+void MapEditor::Fin()
 {
 
+}
+
+
+//------------------------------------
+//          データの保存
+//------------------------------------
+void MapEditor::SaveMap(const char* filename)
+{
+    //-------------------------
+    // 個人作成なのでバイナリで
+    //-------------------------
+    FILE* fp = fopen(filename, "wb");
+    if (fp == NULL) return;
+
+    // サイズも一緒に保存（重要）
+    fwrite(&MAP_W, sizeof(int), 1, fp);
+    fwrite(&MAP_H, sizeof(int), 1, fp);
+
+    // マップ本体
+    fwrite(map, sizeof(TileType), MAP_W * MAP_H, fp);
+
+    fclose(fp);
+}
+
+
+//------------------------------------
+//          マップの読み取り
+//------------------------------------
+void MapEditor::LoadMap(const char* filename)
+{
+
+   //-------------------------
+   // 個人作成なのでバイナリで
+   //-------------------------
+
+    FILE* fp = fopen(filename, "rb");
+    if (fp == NULL) return;
+
+    int w, h;
+    fread(&w, sizeof(int), 1, fp);
+    fread(&h, sizeof(int), 1, fp);
+
+    // サイズチェック（安全対策）
+    if (w != MAP_W || h != MAP_H) {
+        fclose(fp);
+        return;
+    }
+
+    fread(map, sizeof(TileType), MAP_W * MAP_H, fp);
+
+    fclose(fp);
+}
+
+
+//マウスの当たり判定の取得
+bool MapEditor::GetMouseHitPosition(VECTOR* outPos)
+{
+    //@メモ
+    //---------------------------
+   /* レイの式
+    P = near + t * (far - near)
+    y=0 にぶつかる t
+    t = -near.y / (far.y - near.y)*/
+    //-----------------------------
+    int mouseX, mouseY;
+    GetMousePoint(&mouseX, &mouseY);
+
+    VECTOR nearPos, farPos;
+    nearPos= ConvScreenPosToWorldPos(VGet((float)mouseX, (float)mouseY, 0.0f));
+    farPos = ConvScreenPosToWorldPos(VGet((float)mouseX, (float)mouseY, 1.0f));
+
+    VECTOR dir = VSub(farPos, nearPos);
+
+    // 地面と平行ならヒットしない
+    if (fabs(dir.y) < 0.0001f) return false;
+
+    float t = -nearPos.y / dir.y;
+
+    // カメラの後ろは無視
+    if (t < 0.0f) return false;
+
+    // 交点
+    *outPos = VAdd(nearPos, VScale(dir, t));
+
+    return true;
+}
+
+
+//グリッドの取得
+bool MapEditor::GetGridPos(VECTOR hitPos, int* gx, int* gz)
+{
+    *gx = (int)floor(hitPos.x / TILE_SIZE);
+    *gz = (int)floor(hitPos.z / TILE_SIZE);
+
+    // 範囲チェック（超重要）
+    if (*gx < 0 || *gx >= MAP_W ||
+        *gz < 0 || *gz >= MAP_H) {
+        return false;
+    }
+
+    return true;
 }
--- Rhythm_Dungeon/src/game/map/MapEditor.h
+++ Rhythm_Dungeon/src/game/map/MapEditor.h
@@ -18,10 +18,25 @@ public:
 	int  Step();
 	// 描画
 	void Draw();
+	// 更新処理
+	void Update();
 	// 終了処理
 	void Fin();
+	//マップのセーブ
+	void SaveMap(const char* filename);
+	//マップの読み込み
+	void LoadMap(const char* filename);
+	//マウスの当たり判定の取得
+	bool GetMouseHitPosition(VECTOR* outPos);
+	//グリッドの取得
+	bool GetGridPos(VECTOR hitPos, int* gx, int* gz);
 
-	
 private:
 
+	struct RemoveMouse
+	{
+		float mouseX;
+		float mouseY;
+	};
+	//ここに変数を宣言
 };
